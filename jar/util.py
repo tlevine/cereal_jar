935c4d13690b93dbd540c369447d61f2511e2799
Thomas Levine <_@thomaslevine.com>
2014-04-11 02:43:20 +0000

    move

--- jar/util.py
+++ jar/util.py
@@ -1,2 +1,22 @@
 def has_double_percent(text):
     return text.startswith('%%') or '\n%%' in text
+
+def reader(loads, fp):
+    chunk = ''
+    for line in fp:
+        if line.startswith('%%'):
+            yield loads(chunk)
+            chunk = ''
+        else:
+            chunk += line
+
+def writer(dumps, obj, fp):
+    new = True
+    for item in obj:
+        if not new:
+            fp.write('\n%%\n')
+        dumped = dumps(obj)
+        if has_double_percent(dumped):
+            raise ValueError('The (dumped) object must not contain "\n%%".')
+        fp.write(dumped)
+        new = False
