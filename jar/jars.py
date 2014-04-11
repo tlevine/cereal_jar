0c89fd9c81f3cbacfa01b0fb56bce4f240ee8d32
Thomas Levine <_@thomaslevine.com>
2014-04-11 02:51:53 +0000

    jsonjar

--- jar/jars.py
+++ jar/jars.py
@@ -1,8 +1,13 @@
+import json
 import functools
 import jar.util
 
-_identity = lambda x:x
-
 class cookiejar:
-    load = functools.partial(jar.util.reader, _identity)
-    dump = functools.partial(jar.util.writer, _identity)
+    _identity = lambda x:x
+    load = functools.partial(jar.util.reader, self._identity)
+    dump = functools.partial(jar.util.writer, self._identity)
+
+class jsonjar:
+    _dumps = lambda x: json.dumps(x, indent = 2, separators = (',', ': '))
+    load = functools.partial(jar.util.reader, json.loads)
+    dump = functools.partial(jar.util.writer, self._dump)
